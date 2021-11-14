ab1e337e6881dcd8a5d0f1fc684e3ebe2df3e449
RUXANDRA CHISELENCO <ruxandra.chiselenco@stud.ubbcluj.ro>
2021-11-14 17:03:35 +0200

    Rezolvarea Laboratorului 3

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chiselenco_Ruxandra_Lab2
+{
+    class Class1
+    {
+    }
+}
---
+++ Doughnut.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chiselenco_Ruxandra_Lab2
+{
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+        private int mRaisedGlazed = 0;
+        private int mRaisedSugar = 0;
+        private int mFilledLemon = 0;
+        private int mFilledChocolate = 0;
+        private int mFilledVanilla = 0;
+        private double Total = 0;
+        private DoughnutType selectedDoughnut;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mFlavor = aFlavor;
+        }
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+}
